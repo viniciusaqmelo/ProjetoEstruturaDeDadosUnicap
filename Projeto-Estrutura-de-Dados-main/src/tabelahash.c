143ad31659d3ab89da72800773b7ba2160a83f22
Vinicius Aquino <104593559+viniciusaqmelo@users.noreply.github.com>
2025-05-25 19:39:38 +0000

    alteração de arquivo

--- Projeto-Estrutura-de-Dados-main/src/tabelahash.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include "tabelahash.h"
-
-int hashString(const char* str) {
-    int hash = 0;
-    while (*str)
-        hash = (hash * 31 + *str++) % TAMANHO_TABELA;
-    return hash;
-}
